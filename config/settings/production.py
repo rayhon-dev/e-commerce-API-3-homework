d238c22f6d0180b1fb27ab43d9cdab5f1652b084
Rayhon Irisqulova <rayhonirisqulova@gmail.com>
2025-06-21 00:22:33 +0500

    fix: update ALLOWED_HOSTS and CORS settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,6 +2,7 @@ from .base import *  # noqa
 
 ALLOWED_HOSTS = [
     "3-project.midnight-pages.uz",
+    ".vercel.app",
     "localhost",
     "127.0.0.1",
     "5.182.26.14"
@@ -19,7 +20,6 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SESSION_COOKIE_SECURE = True
 SECURE_HSTS_PRELOAD = True
 CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOW_CREDENTIALS = False
 CORS_ORIGIN_ALLOW_ALL = False
 
 REST_FRAMEWORK.update(  # noqa: F405
