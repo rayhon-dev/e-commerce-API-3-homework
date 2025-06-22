f7337f7a9a6ee88e5dcb8ad728da0bc41d6153ea
Rayhon Irisqulova <rayhonirisqulova@gmail.com>
2025-06-22 14:31:21 +0500

    Some features changed

--- config/settings/base.py
+++ config/settings/base.py
@@ -77,7 +77,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "config.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.app"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
