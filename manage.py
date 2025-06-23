ba78ea47d6c39b724921f03a032fcfdadfadab0b
Rayhon Irisqulova <rayhonirisqulova@gmail.com>
2025-06-23 21:57:18 +0500

    Some features changed

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 def main():
     """Run administrative tasks."""
     os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "config.settings.production"
+        "DJANGO_SETTINGS_MODULE", "config.settings.development"
     )
     try:
         from django.core.management import execute_from_command_line
